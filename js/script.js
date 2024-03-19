2da9405363d90d53281f4507992e41ab677919ec
MdSojol <rsojol8888@gmail.com>
2024-03-19 21:50:26 +0600

    changes

--- js/script.js
+++ js/script.js
@@ -1,21 +1,39 @@
-function triangleBaseInputValue(triangleBaseInputId){
+function triangleBaseInputValue(triangleBaseInputId) {
     document.getElementById('triangleBaseInputId');
     const triangleBaseInputValueConvart = triangleBaseInputId.value;
     const triangleBaseInputValueConvertStringToNumber = parseFloat(triangleBaseInputValueConvart);
-    triangleBaseInputId.value ="";
+    triangleBaseInputId.value = "";
     return triangleBaseInputValueConvertStringToNumber;
 }
 
-function triangleBaseTextToNumber(stringToNumer){
+function triangleBaseTextToNumber(stringToNumer) {
     document.getElementById('stringToNumer');
-    const triangleBaseTextCapture =stringToNumer.innerText;
+    const triangleBaseTextCapture = stringToNumer.innerText;
     const triangleBaseTextToNumberCapture = parseFloat(triangleBaseTextCapture);
     return triangleBaseTextToNumberCapture;
 }
 
-document.getElementById('triangleButtons').addEventListener('click', function(){
+function rightSiteValuPrinting(valueeessm, area) {
+
+    /* const para = document.createElement("p");
+    const node = document.createTextNode(valueeess);
+    const paraAndNodeRetuns = para.appendChild(node);
+    return paraAndNodeRetuns; */
+    const calculationEntery = document.getElementById('rightSideId');
+    const p = document.createElement('p');
+    p.innerHTML = valueeessm + " " + area;
+    calculationEntery.appendChild(p);
+    
+    
+    
+    /* 
+    const element = document.getElementById("rightSideId");
+    element.appendChild(para); */
+}
+
+document.getElementById('triangleButtons').addEventListener('click', function () {
     const triangleClickValuBase = triangleBaseInputValue(triangleBaseInput);
-    const triangleClickValuHeight =triangleBaseInputValue(TriangleHeightInput);
+    const triangleClickValuHeight = triangleBaseInputValue(TriangleHeightInput);
     const triangleTotalValue = triangleClickValuBase * triangleClickValuHeight;
     // console.log(triangleTotalValue)
 
@@ -26,80 +44,87 @@ document.getElementById('triangleButtons').addEventListener('click', function(){
     const totalAreaSet = triangleBaseTextToNumber(totalArea);
     const totalAreaValuConcat = totalAreaSet + trianglePresentValuAndInputValuTotal
     const totalAreaSetId = document.getElementById('totalArea');
-    if(isNaN(triangleClickValuBase) || isNaN(triangleClickValuHeight)){
+    if (isNaN(triangleClickValuBase) || isNaN(triangleClickValuHeight)) {
         alert('please give me a numbers');
         return;
     }
     totalAreaSetId.innerText = totalAreaValuConcat;
+    rightSiteValuPrinting("Triangle : ", totalAreaValuConcat);
     
-    
+
+
 
 
 })
 
-document.getElementById('SecondButtons').addEventListener('click', function(){
+document.getElementById('SecondButtons').addEventListener('click', function () {
     const secondInputFastValueCapture = triangleBaseInputValue(SecondSectionFastInputs);
     const secondInputSecondValueCapture = triangleBaseInputValue(SecondSectionSecondInputs);
-    const totalSecondSectionValue =  secondInputFastValueCapture * secondInputSecondValueCapture;
+    const totalSecondSectionValue = secondInputFastValueCapture * secondInputSecondValueCapture;
     const secondSectionValuSets = document.getElementById('SecondTotalAreaSecond');
-    secondSectionValuSets.innerText= totalSecondSectionValue;
+    secondSectionValuSets.innerText = totalSecondSectionValue;
+    rightSiteValuPrinting("Rectangle : ", totalSecondSectionValue);
 
 })
 
 
 
-function thirdSection(){
+function thirdSection() {
     const thridSectionFastValue = triangleBaseInputValue(thridInputsFastValue);
     const thridSectionSecondValue = triangleBaseInputValue(thridInputsSecondValue);
-    const totalThridValue = thridSectionFastValue*thridSectionSecondValue;
+    const totalThridValue = thridSectionFastValue * thridSectionSecondValue;
     return totalThridValue;
 }
 
 
-function forthSection(forthInputValuFast,forthInputValuSecond,forthSectionFixtValu){
+function forthSection(forthInputValuFast, forthInputValuSecond, forthSectionFixtValu) {
     const thridSectionFastValue = triangleBaseInputValue(forthInputValuFast);
     const thridSectionSecondValue = triangleBaseInputValue(forthInputValuSecond);
     const thridSectionSecondValueToNumber = triangleBaseTextToNumber(forthSectionFixtValu);
-    const totalThridValue = thridSectionFastValue*thridSectionSecondValue;
+    const totalThridValue = thridSectionFastValue * thridSectionSecondValue;
     const totalAllok = totalThridValue * thridSectionSecondValueToNumber;
     return totalAllok;
 }
 
-function forthSectionSetString(elementID, area){
+function forthSectionSetString(elementID, area) {
     const element = document.getElementById(elementID);
     element.innerText = area;
 }
 
 
-document.getElementById('thirdButtons').addEventListener('click', function(){
+document.getElementById('thirdButtons').addEventListener('click', function () {
     const thridValuRetunCapture = thirdSection();
     const thridSetValuStringToNumber = triangleBaseTextToNumber(thirdTotalAreaSecond);
-    const totalThirdValu =  thridSetValuStringToNumber + thridValuRetunCapture;
+    const totalThirdValu = thridSetValuStringToNumber + thridValuRetunCapture;
     const thridSectionSetValu = document.getElementById('thirdTotalAreaSecond')
     thridSectionSetValu.innerText = totalThirdValu;
+    rightSiteValuPrinting("Parallelogram : ", totalThirdValu);
 
 })
 
 
-document.getElementById('forthButtons').addEventListener('click', function(){
-   const retunValu = forthSection(forthInputValuFast,forthInputValuSecond,forthSectionFixtValu);
-   forthSectionSetString("forthTotalAreaSecond",retunValu)
+document.getElementById('forthButtons').addEventListener('click', function () {
+    const retunValu = forthSection(forthInputValuFast, forthInputValuSecond, forthSectionFixtValu);
+    forthSectionSetString("forthTotalAreaSecond", retunValu);
+    rightSiteValuPrinting("Rhombus : ", retunValu);
 })
 
 
-document.getElementById('fifth-button').addEventListener('click', function(){
-   const retunValu = forthSection(fifthFastInputs,fifthSecondInputs,fifthFixtValus);
-   forthSectionSetString("fifthTotalAreaSecond",retunValu)
+document.getElementById('fifth-button').addEventListener('click', function () {
+    const retunValu = forthSection(fifthFastInputs, fifthSecondInputs, fifthFixtValus);
+    forthSectionSetString("fifthTotalAreaSecond", retunValu);
+    rightSiteValuPrinting("Pentagon : ", retunValu);
 })
 
 
-document.getElementById('sixButtons').addEventListener('click', function(){
+document.getElementById('sixButtons').addEventListener('click', function () {
     const sixinputsfast = triangleBaseInputValue(sixFirstInputs);
     const sixinputsSecond = triangleBaseInputValue(sixSecondInputs);
     const sixInputTotals = sixinputsfast * sixinputsSecond;
     const sixTotalPi = sixInputTotals * 3.14159265359;
-    forthSectionSetString("sixTotalAreaSecond", sixTotalPi)
-    
+    forthSectionSetString("sixTotalAreaSecond", sixTotalPi);
+    rightSiteValuPrinting("Ellipse : ", sixTotalPi);
+
 })
 
 
